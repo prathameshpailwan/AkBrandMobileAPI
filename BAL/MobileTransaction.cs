59d3741207ebee9dc79b5a7337fd306cfaaaa427
prathameshpailwan <prathameshpailwan02@gmail.com>
2025-04-25 22:42:51 +0530

    Add project files.

---
+++ BAL/MobileTransaction.cs
@@ -0,0 +1,110 @@
+﻿using AkBrandMobile.Common;
+using AkBrandMobile.DAL;
+using AkBrandMobile.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace AkBrandMobile.BAL
+{
+    public class MobileTransaction
+    {
+
+        private readonly MobileDbConnection _mobileDbConnection;
+        private readonly ConversionsClass _conversions;
+
+
+        public MobileTransaction(MobileDbConnection mobileDbConnection, ConversionsClass conversions)
+        {
+            _mobileDbConnection = mobileDbConnection;
+            _conversions = conversions;
+        }
+        public JsonResult GetMobileData()
+        {
+
+            var response = _mobileDbConnection.ExecuteStoredProcedure("GetMobileData", null, true);
+            if (response != null)
+            {
+                return new JsonResult(response);
+
+            }
+            else
+            {
+                return new JsonResult("No Data Found");
+            }
+        }
+
+        public JsonResult GetAccesories()
+        {
+            var response = _mobileDbConnection.ExecuteStoredProcedure("GetAccessories", null, true);
+            if (response != null)
+            {
+                return new JsonResult(response);
+            }
+            else
+            {
+                return new JsonResult("No Data Found");
+            }
+        }
+
+        public JsonResult InsertUpdate_Accesories(AccessoryModel accessoryModel)
+        {
+            var parameters = _conversions.CreateParametersFromModel(accessoryModel);
+            DataTable detailTable = null;
+            var response = _mobileDbConnection.ExecuteStoredProcedure("Accessories_Insert_Update", parameters, false);
+            if (response != null)
+            {
+                return new JsonResult(response);
+
+            }
+            else
+            {
+                return new JsonResult("No Data Found");
+            }
+        }
+
+        public JsonResult AddTypeCompanyColour(TypeCompanyColourModel typeCompanyColour)
+        {
+            var parameters = _conversions.CreateParametersFromModel(typeCompanyColour);
+            DataTable detailTable = null;
+            var response = _mobileDbConnection.ExecuteStoredProcedure("InsertMobileType_Company_Colour", parameters, false);
+            if (response != null)
+            {
+                return new JsonResult(response);
+
+            }
+            else
+            {
+                return new JsonResult("No Data Found");
+            }
+        }
+
+        public JsonResult MobileModelTransaction(ModelTransaction modelTransaction)
+        {
+            var parameters = _conversions.CreateParametersFromModel(modelTransaction);
+            DataTable detailTable = null;
+            var response = _mobileDbConnection.ExecuteStoredProcedure("MobileModelTransaction", parameters, false);
+            if (response != null)
+            {
+                return new JsonResult(response);
+
+            }
+            else
+            {
+                return new JsonResult("No Data Found");
+            }
+        }
+        public JsonResult GetMobileMasterData()
+        {
+            var response = _mobileDbConnection.ExecuteStoredProcedure("Get_MobileType_Company_Colour", null, true);
+            if (response != null)
+            {
+                return new JsonResult(response);
+            }
+            else
+            {
+                return new JsonResult("No Data Found");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using AkBrandMobile.BAL;
+using AkBrandMobile.Common;
+using AkBrandMobile.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<MobileTransaction>(); // MobileTransaction service
+builder.Services.AddScoped<MobileDbConnection>(); // MobileDbConnection service
+builder.Services.AddScoped<ConversionsClass>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ✅ Configure CORS Policy (UPDATED)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // ✅ Allow only Angular app (secure)
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials(); // ✅ Allow authentication cookies/tokens
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
